77a0ac51e9a552a52bdd99eb8704215da52b528e
jhon saldarriaga <jhonalex21@gmail.com>
2023-09-04 10:48:32 -0500

    listo

---
+++ js/main.js
@@ -0,0 +1,50 @@
+
+window.onload =()=>{
+  const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+    autoplay: true,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+    // And if we need scrollbar
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+  });
+
+  let btm, frm, txt; //variables
+
+window.onload=()=>{
+  btn= document.getElementById("action");
+  btn.adEventListener("click",enviar);
+}
+
+function enviar(event){
+  frm= document.GetElementById("formulario");
+  if(fmr.chekValidity());
+{
+txt = document.getElementById("suscripcion");
+txt.value="";
+alert("Bienvenido. Comienza a disfrutar de los beneficios");
+event.preventDefault();
+}
+}  
+
+
+
+
+
+
+}
+
